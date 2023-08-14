ba44715994fa556a8f113c89193836a1c8ba69fc
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-14 14:20:20 -0700

    Update App.JS

--- App.js
+++ App.js
@@ -1,1 +1,16 @@
-asd
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Gallery from './Gallery';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Gallery />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
