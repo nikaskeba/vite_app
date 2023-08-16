86c41a63b9b00c0e74baf0591e2f13f45638aba6
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-15 18:31:36 -0700

    Updated gallery to data feature
    Add favorite, add data feature, add heart

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,23 +1,25 @@
 import React from 'react';
+import data from './data.json'; // adjust the path if the JSON is in a different location
 import HornedBeast from './HornedBeast';
 import beast1Image from './assets/bowling1.jpeg';
 import beast2Image from './assets/hornedtennis.jpeg';
 function Gallery() {
   return (
-    <div>
-      <HornedBeast
-        title="The Horned Beast Bowler"
-        imageUrl={beast1Image}
-        description="Horned Beast that loves to bowl."
-      />
-      <HornedBeast
-        title="Beast Tennis Player"
-        imageUrl={beast2Image}
-        description="Horned Beast that plays tennis."
-      />
-     
+  
+  
+      <div>
+      {data.map(beast => (
+        <HornedBeast
+          key={beast._id}
+          title={beast.title}
+          imageUrl={beast.image_url}
+          description={beast.description}
+        />
+      ))}
     </div>
+ 
   );
 }
 
 export default Gallery;
+
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function HornedBeast(props) {
+  // Initialize state for favorites
+  const [favorites, setFavorites] = useState(0);
+
+  // Function to handle click and update favorites count
+  const handleFavorite = () => {
+    setFavorites(favorites + 1);
+  };
+
   return (
     <div>
       <h2>{props.title}</h2>
-      <img style={{width: '200px'}} src={props.imageUrl} alt={props.title} title={props.title} />
+      <img style={{width: '100px'}} src={props.imageUrl} alt={props.title} title={props.title} />
       <p>{props.description}</p>
+      <p>
+        <span role="img" aria-label="heart" onClick={handleFavorite}>
+          💘
+        </span>
+        Favorites: {favorites}
+      </p>
     </div>
   );
 }
 
 export default HornedBeast;
+
+
