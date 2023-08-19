bf8d01693530cba67eb7effdd025f5a8c2811715
Nika Skeba <67114318+nikaskeba@users.noreply.github.com>
2023-08-19 15:42:43 -0700

    Added drop down feature

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import data from './data.json'; // adjust the path if the JSON is in a different location
+import React, { useState } from 'react';
 import HornedBeast from './HornedBeast';
-import { Card} from 'react-bootstrap';
+import data from './data.json';
+import { Container, Row, Col, Form } from 'react-bootstrap';
+
 function Gallery() {
-  return (
-  <Card>
-  <Card.Body>
-      <div>
-      {data.map(beast => (
-        <HornedBeast
-          key={beast._id}
-          title={beast.title}
-          imageUrl={beast.image_url}
-          description={beast.description}
-        />
-      ))}
-    </div>
- </Card.Body>
-  </Card>
-  );
+    const [selectedHorns, setSelectedHorns] = useState("all");
+
+    return (
+        <Container fluid>
+            <Form>
+                <Form.Group controlId="hornsSelect">
+                    <Form.Label>Select number of horns</Form.Label>
+                    <Form.Control 
+                        as="select" 
+                        value={selectedHorns} 
+                        onChange={(e) => setSelectedHorns(e.target.value)}
+                    >
+                        <option value="all">All</option>
+                        <option value="1">1 Horn</option>
+                        <option value="2">2 Horns</option>
+                        {/* Add more options as needed */}
+                    </Form.Control>
+                </Form.Group>
+            </Form>
+
+            <Row>
+                {data.filter(beast => selectedHorns === "all" || beast.horns === parseInt(selectedHorns))
+                     .map(beast => (
+                         <Col md={4} sm={6} xs={12} key={beast._id}>
+                             <HornedBeast
+                                 title={beast.title}
+                                 imageUrl={beast.image_url}
+                                 description={beast.description}
+                             />
+                         </Col>
+                     ))}
+            </Row>
+        </Container>
+    );
 }
 
 export default Gallery;
